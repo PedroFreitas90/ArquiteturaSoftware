603d538a984fc0a30e8f4afc911dcbe8eddc84c6
PedroFreitas90 <pedrofreitas1410@gmail.com>
2019-12-12 22:48:55 +0000

    Fim

--- ArqSw/src/main/java/Servidor/Observer.java
+++
@@ -1,5 +0,0 @@
-package Servidor;
-
-public interface Observer {
-    public void update(Ativo a);
-}
--- ArqSw/src/main/java/Servidor/UtilizadorInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package Servidor;
-
-public class UtilizadorInvalidoException extends Exception {
-
-    public UtilizadorInvalidoException(String msg){
-        super(msg);
-    }
-
-}
